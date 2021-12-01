27349df45155bdab5c9a7b3c2881a2f1f2181c12
jsist <jsistop16@naver.com>
2021-12-01 18:46:46 +0900

    calendar에서 날씨 기상청 link추가

--- app/src/main/java/com/example/se_demo/FragmentCalendar.java
+++ app/src/main/java/com/example/se_demo/FragmentCalendar.java
@@ -1,6 +1,8 @@
 package com.example.se_demo;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,106 +35,21 @@ import java.util.Map;
 
 public class FragmentCalendar extends Fragment {
 
-//    View view;
-//    TextView cityView = view.findViewById(R.id.cityView);
-//    TextView weatherView = view.findViewById(R.id.weatherView);
-//    TextView tempView = view.findViewById(R.id.tempView);
-//    static RequestQueue requestQueue;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
-
-//        Button button = view.findViewById(R.id.weatherStart);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                CurrentCall();
-//            }
-//        });
-//
-//        if(requestQueue == null){
-//            requestQueue = Volley.newRequestQueue(getActivity());
-//        }
-
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        Button weather = (Button) view.findViewById(R.id.weatherlink);
+
+
+
+        weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.weather.go.kr/w/index.do"));
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
-//    private void CurrentCall(){
-//
-//        String url = "http://api.openweathermap.org/data/2.5/weather?q={seoul}&appid={93804ba597ecd27e84a40ab55205cb11}";
-//
-//
-//        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-//
-//            @SuppressLint("SetTextI18n")
-//            @Override
-//            public void onResponse(String response) {
-//
-//                try {
-//
-//                    //System의 현재 시간(년,월,일,시,분,초까지)가져오고 Date로 객체화함
-//                    long now = System.currentTimeMillis();
-//                    Date date = new Date(now);
-//
-//                    //년, 월, 일 형식으로. 시,분,초 형식으로 객체화하여 String에 형식대로 넣음
-//                    SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
-//                    SimpleDateFormat simpleDateFormatTime = new SimpleDateFormat("HH:mm:ss");
-//                    String getDay = simpleDateFormatDay.format(date);
-//                    String getTime = simpleDateFormatTime.format(date);
-//
-//
-//
-//                    //api로 받은 파일 jsonobject로 새로운 객체 선언
-//                    JSONObject jsonObject = new JSONObject(response);
-//
-//
-//                    //도시 키값 받기
-//                    String city = jsonObject.getString("name");
-//
-//                    cityView.setText(city);
-//
-//
-//                    //날씨 키값 받기
-//                    JSONArray weatherJson = jsonObject.getJSONArray("weather");
-//                    JSONObject weatherObj = weatherJson.getJSONObject(0);
-//
-//                    String weather = weatherObj.getString("description");
-//
-//                    weatherView.setText(weather);
-//
-//
-//
-//                    //기온 키값 받기
-//                    JSONObject tempK = new JSONObject(jsonObject.getString("main"));
-//
-//                    //기온 받고 켈빈 온도를 섭씨 온도로 변경
-//                    double tempDo = (Math.round((tempK.getDouble("temp")-273.15)*100)/100.0);
-//                    tempView.setText(tempDo +  "°C");
-//
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//
-//            }
-//        }){
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String, String> params = new HashMap<String, String>();
-//
-//                return params;
-//            }
-//
-//        };
-//
-//        request.setShouldCache(false);
-//        requestQueue.add(request);
-//    }
-    
 
 }
